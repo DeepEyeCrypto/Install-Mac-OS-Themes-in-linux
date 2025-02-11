821a631166d6f2ebe6fc06548f4ffe5a034e87b1
DeepEyeCrypto <nazzo.np@gmail.com>
2025-02-12 03:21:09 +0530

    Update install-macos-theme.sh

--- install-macos-theme.sh
+++ install-macos-theme.sh
@@ -3,34 +3,66 @@
 # macOS Theme Install Script for Kali Linux
 # Requires GNOME Desktop Environment
 
+LOGFILE=install-macos-theme.log
+
+# Function to log messages
+log() {
+    echo "$(date +'%Y-%m-%d %H:%M:%S') - $1" | tee -a $LOGFILE
+}
+
+# Function to check if a command exists
+command_exists() {
+    command -v "$1" >/dev/null 2>&1
+}
+
 # Exit on error and print commands
 set -e
+trap 'log "An error occurred. Exiting..."' ERR
 
-# Install necessary dependencies
-sudo apt update
-sudo apt install -y git curl gnome-tweaks gnome-shell-extensions plank
+log "Starting macOS theme installation..."
+
+# Confirm with the user
+read -p "This will modify your GNOME settings. Do you want to continue? (y/n): " confirm
+if [[ "$confirm" != "y" ]]; then
+    log "Installation aborted by user."
+    exit 1
+fi
+
+# Backup existing GNOME settings
+log "Backing up existing GNOME settings..."
+dconf dump /org/gnome/ > gnome-settings-backup.dconf
+
+# Check and install necessary dependencies
+log "Checking for necessary dependencies..."
+DEPS=(git curl gnome-tweaks gnome-shell-extensions plank unzip)
+for dep in "${DEPS[@]}"; do
+    if ! command_exists $dep; then
+        log "Installing missing dependency: $dep"
+        sudo apt install -y $dep
+    fi
+done
 
 # Download and install WhiteSur GTK theme
-echo "Installing WhiteSur GTK theme..."
+log "Installing WhiteSur GTK theme..."
 git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git
-WhiteSur-gtk-theme/install.sh -t all -c light -s 220 -i standard --dest ~/.themes
+./WhiteSur-gtk-theme/install.sh -t all -c light -s 220 -i standard --dest ~/.themes
 rm -rf WhiteSur-gtk-theme
 
 # Install McMojave Circle icons
-echo "Installing McMojave icons..."
+log "Installing McMojave Circle icons..."
 git clone https://github.com/vinceliuice/McMojave-circle.git
-McMojave-circle/install.sh --dest ~/.icons
+./McMojave-circle/install.sh --dest ~/.icons
 rm -rf McMojave-circle
 
 # Install Capitaine Cursors
-echo "Installing Capitaine Cursors..."
+log "Installing Capitaine Cursors..."
 git clone https://github.com/keeferrourke/capitaine-cursors.git
 mkdir -p ~/.icons/Capitaine
 cp -r capitaine-cursors/dist/* ~/.icons/Capitaine/
 rm -rf capitaine-cursors
 
 # Install San Francisco Font
-echo "Installing San Francisco Font..."
+log "Installing San Francisco Font..."
 mkdir -p ~/.local/share/fonts/SFPro
 curl -L https://github.com/sahibjotsaggu/San-Francisco-Pro-Fonts/archive/master.zip -o SFPro.zip
 unzip SFPro.zip -d ~/.local/share/fonts/SFPro/
@@ -39,26 +71,27 @@ rm -rf SFPro.zip ~/.local/share/fonts/SFPro/San-Francisco-Pro-Fonts-master
 fc-cache -fv
 
 # Set theme settings
-echo "Configuring theme settings..."
+log "Configuring theme settings..."
 gsettings set org.gnome.desktop.interface gtk-theme "WhiteSur-light"
 gsettings set org.gnome.desktop.interface icon-theme 'McMojave-circle'
 gsettings set org.gnome.desktop.interface cursor-theme 'Capitaine'
 gsettings set org.gnome.desktop.interface font-name 'SF Pro Display 10'
 
 # Set macOS wallpaper (replace URL with actual macOS wallpaper)
-echo "Setting up wallpaper..."
+log "Setting up wallpaper..."
 WALLPAPER_URL="https://raw.githubusercontent.com/thanhtamkaito/SettingOpenVCS/master/macOS/monterey.jpg"
 mkdir -p ~/Pictures/macOS-wallpaper
 curl -L "$WALLPAPER_URL" -o ~/Pictures/macOS-wallpaper/desktop.jpg
 gsettings set org.gnome.desktop.background picture-uri "file://$HOME/Pictures/macOS-wallpaper/desktop.jpg"
 
 # Configure Plank dock
-echo "Setting up Plank dock..."
+log "Setting up Plank dock..."
 mkdir -p ~/.config/plank/dock1/launchers
 cp /usr/share/applications/org.gnome.Terminal.desktop ~/.config/plank/dock1/launchers/
 cp /usr/share/applications/firefox-esr.desktop ~/.config/plank/dock1/launchers/
 
 # Add Plank to autostart
+log "Adding Plank to autostart..."
 mkdir -p ~/.config/autostart
 echo "[Desktop Entry]
 Name=Plank
@@ -67,6 +100,7 @@ Type=Application
 " > ~/.config/autostart/plank.desktop
 
 # Enable GNOME extensions
+log "Enabling GNOME extensions..."
 gnome-extensions enable user-theme@gnome-shell-extensions.gcampax.github.com
 
-echo "Installation complete! Log out and log back in to apply all changes."
+log "Installation complete! Log out and log back in to apply all changes."
